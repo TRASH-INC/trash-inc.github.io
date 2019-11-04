b9952197f85e0fb4ab535d2909c4a37258f99b5c
Genevieve Patterson <gen@trash.app>
2019-11-04 15:20:21 -0500

    check if browser is safari

--- js/ioscheck.js
+++ js/ioscheck.js
@@ -6,16 +6,17 @@ function iOScheck() {
 }
 
 isiOS = iOScheck();
+isSafari = navigator.userAgent.indexOf("Safari") > -1;
 
-if (isiOS) {
+if (isiOS && isSafari) {
     // remove the download from app store button if this is in iOS
     // because there will be a smart banner
     document.getElementById("appstore1").remove();
 
     // add download link in footer incase the
     // smart banner was dismissed
-    footer = document.getElementById("menu");
-    dlLink = document.createElement('h4');
+    var footer = document.getElementById("menu");
+    var dlLink = document.createElement('h4');
     footer.prepend(dlLink);
     dlLink.outerHTML = '<h4><a href="https://apps.apple.com/us/app/trash-you-shoot-we-edit/id1412091383?ls=1" >DOWNLOAD APP</a></h4><br>';
 
